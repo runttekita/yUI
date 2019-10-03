5478a11649c91819932eab242e6c7512fa703a26
velvet-halation <buizark@gmail.com>
2019-10-03 08:34:59 -0700

    render coords

--- src/main/kotlin/reina/yUi/YuiClickableObject.kt
+++ src/main/kotlin/reina/yUi/YuiClickableObject.kt
@@ -2,16 +2,15 @@ package reina.yUi
 
 import basemod.ClickableUIElement
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.evacipated.cardcrawl.mod.stslib.patches.HitboxRightClick
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.helpers.FontHelper
 import com.megacrit.cardcrawl.helpers.input.InputHelper
 
-abstract class YuiClickableObject(private var texture: Texture) : ClickableUIElement(texture) {
-    private var inMoveMode = false
-    init{
-
-    }
+abstract class YuiClickableObject(private val texture: Texture) : ClickableUIElement(texture) {
+    private var inMoveMode: Boolean = false
 
     override fun updateHitbox() {
         super.updateHitbox()
@@ -26,6 +25,14 @@ abstract class YuiClickableObject(private var texture: Texture) : ClickableUIEle
         y = InputHelper.mY.toFloat();
         if (InputHelper.justClickedLeft)
             inMoveMode = false
+    }
+
+    override fun render(sb: SpriteBatch?) {
+        super.render(sb)
+        if (Settings.isDebug)
+            FontHelper.renderFontCentered(sb, FontHelper.energyNumFontPurple, x.toString(),
+                texture.width.toFloat() / Settings.scale, texture.height.toFloat() / Settings.scale
+            )
     }
 
 }
