2a3e1efe954e88bd41db0358f6ceb7e1880f502b
velvet-halation <buizark@gmail.com>
2019-10-03 08:29:07 -0700

    move mode

--- src/main/kotlin/reina/yUi/YuiClickableObject.kt
+++ src/main/kotlin/reina/yUi/YuiClickableObject.kt
@@ -2,11 +2,30 @@ package reina.yUi
 
 import basemod.ClickableUIElement
 import com.badlogic.gdx.graphics.Texture
+import com.evacipated.cardcrawl.mod.stslib.patches.HitboxRightClick
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.helpers.input.InputHelper
 
 abstract class YuiClickableObject(private var texture: Texture) : ClickableUIElement(texture) {
-
+    private var inMoveMode = false
     init{
 
+    }
+
+    override fun updateHitbox() {
+        super.updateHitbox()
+        moveMode()
+        if (HitboxRightClick.rightClicked.get(this) && Settings.isDebug)
+            inMoveMode = true
+    }
+
+    private fun moveMode() {
+        if (!inMoveMode) return;
+        x = InputHelper.mX.toFloat()
+        y = InputHelper.mY.toFloat();
+        if (InputHelper.justClickedLeft)
+            inMoveMode = false
     }
 
 }
