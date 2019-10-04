1a8a47f8da8cd7a03b293a58aa872f5cd2e43a3e
velvet-halation <buizark@gmail.com>
2019-10-03 17:36:35 -0700

    stop move mode

--- src/main/kotlin/reina/yUi/YuiClickableObject.kt
+++ src/main/kotlin/reina/yUi/YuiClickableObject.kt
@@ -19,6 +19,8 @@ abstract class YuiClickableObject(private val texture: Texture, x: Float, y: Flo
     init {
         this.x = x
         this.y = y
+        hitbox.x = x
+        hitbox.y = y
     }
 
     override fun onHover() {
@@ -37,6 +39,8 @@ abstract class YuiClickableObject(private val texture: Texture, x: Float, y: Flo
             y = InputHelper.mY.toFloat()
             hitbox.x = x
             hitbox.y = y
+            if (inputMove.isJustPressed)
+                inMoveMode = !inMoveMode
         }
     }
 
