ab426ffe2811c22a92c7e87d267fa04770b45cca
velvet-halation <buizark@gmail.com>
2019-10-03 18:19:38 -0700

    javadocs and nudge

--- src/main/kotlin/reina/yUi/YuiClickableObject.kt
+++ src/main/kotlin/reina/yUi/YuiClickableObject.kt
@@ -12,10 +12,25 @@ import com.megacrit.cardcrawl.helpers.FontHelper
 import com.megacrit.cardcrawl.helpers.input.InputAction
 import com.megacrit.cardcrawl.helpers.input.InputHelper
 
+/**
+ * @param texture The image you want to draw.
+ * @param x The x value of where you want to draw it.
+ * @param y The y value of where you want to draw it.
+ *
+ * Features:
+ * Move Mode:
+ *  Press L while hovering over your yUi Element in debug mode to make it snap to your mouse
+ *  and display the x, y values you want to put into x/
+ *  You may also use the arrow keys to nudge it ever so slightly.
+ */
 abstract class YuiClickableObject(private val texture: Texture, x: Float, y: Float) :
     ClickableUIElement(texture, x, y, texture.width.toFloat(), texture.height.toFloat()) {
     public var inMoveMode= false
-    private val inputMove = InputAction(Input.Keys.Q)
+    private val inputMove = InputAction(Input.Keys.L)
+    private val inputUp = InputAction(Input.Keys.UP)
+    private val inputRight = InputAction(Input.Keys.RIGHT)
+    private val inputLeft = InputAction(Input.Keys.LEFT)
+    private val inputDown = InputAction(Input.Keys.DOWN)
     private var waitTimer = 0.5f
     private var xValue = x / Settings.scale
     private var yValue = y / Settings.scale
@@ -45,6 +60,18 @@ abstract class YuiClickableObject(private val texture: Texture, x: Float, y: Flo
         if (inMoveMode) {
             x = InputHelper.mX.toFloat()
             y = InputHelper.mY.toFloat()
+            if (inputUp.isPressed) {
+                y += 10 * Settings.scale
+            }
+            if (inputDown.isPressed) {
+                y -= 10 * Settings.scale
+            }
+            if (inputRight.isPressed) {
+                x += 10 * Settings.scale
+            }
+            if (inputLeft.isPressed) {
+                x -= 10 * Settings.scale
+            }
             hitbox.x = x
             hitbox.y = y
             waitTimer -= Gdx.graphics.deltaTime
