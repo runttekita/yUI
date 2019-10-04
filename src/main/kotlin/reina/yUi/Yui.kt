5f68e80b67b6a97f895270583c6a6c44e1157047
velvet-halation <buizark@gmail.com>
2019-10-04 01:41:01 -0700

    typo

--- src/main/kotlin/reina/yUi/Yui.kt
+++ src/main/kotlin/reina/yUi/Yui.kt
@@ -49,7 +49,7 @@ class Yui() :
      */
     override fun receiveRender(sb: SpriteBatch) {
         if (AbstractDungeon.player != null && inputSpawnYui!!.isJustPressed) {
-            Yui.add(SimpleYuiObject())
+            add(SimpleYuiObject())
         }
         for (yui in listOfYui) {
             yui.render(sb)
