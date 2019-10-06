1ce2d4518e6f04a11aaf25e1f74dfa8105b9937b
velvet-halation <buizark@gmail.com>
2019-10-06 03:27:39 -0700

    upload

--- release.sh
+++ release.sh
@@ -5,3 +5,4 @@ mvn package
 mv ./mods/yUi.jar ./workshop/content/
 cp ./workshop/content/yUi.jar ./workshop/content/idea/yUi.jar
 zip -d ./workshop/content/idea/yUi.jar kotlin/*
+java -jar ../.local/share/Steam/steamapps/common/SlayTheSpire/mod-uploader.jar upload -w=workshop
